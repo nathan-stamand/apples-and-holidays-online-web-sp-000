04ecd393dadc8dbfee1495bc93bb9ebda1ab90ec
Nathan St.Amand <nathansaint810@gmail.com>
2020-02-22 20:03:20 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -81,7 +81,14 @@ end
 def all_holidays_with_bbq(holiday_hash)
   # return an array of holiday names (as symbols) where supply lists
   # include the string "BBQ"
-
+  bbq_days = []
+  holiday_hash.each do |season, holiday|
+    holiday.each do |holiday, array|
+      array.any?("BBQ")
+      bbq_days << holiday
+    end
+  end
+  bbq_days
 end
 
 
